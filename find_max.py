261f5aa8e1d1e2eb336277a290cea92c10c07f2b
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-20 19:36:16 +0530

    Update and rename README.md to find_max.py

---
+++ find_max.py
@@ -0,0 +1,11 @@
+# find_max
+def main():
+    pass
+    l=map(int,input().split())
+    k=int(0)
+    for i in l:
+        if (k<i):
+            k=i
+    print(k)
+if __name__ == '__main__':
+    main()
